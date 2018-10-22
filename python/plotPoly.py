869ba49b6a8e67c30f7f3806224e839acc802c51
mgrigola <grigola3@gmail.com>
2018-10-22 07:15:52 -0500

    syncing workspace file

--- python/plotPoly.py
+++ python/plotPoly.py
@@ -2,37 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import fiona
 
-#with fiona.open('cb_2015_us_zcta510_500k.shp', 'r') as src:
-with fiona.open("C:\Python34\m\WI_County\cb_2015_55_cousub_500k.shp", 'r') as src:
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-#	print(f['geometry'])
-#	print('Type: ', f['type'])
-#	print('ID: ', f['id'])
-#	
-#	for wut in f:
-#		print(wut)
-	cnt=0
-	for feature in src:	
-		for poly in feature['geometry']['coordinates']:
-			coords = np.array(poly).squeeze()
-			#coords=coords[coords[:,0]<0]
-			cnt=cnt+1
-			#print(feature['id'], coords.shape, feature['properties']['ZCTA5CE10'], feature['geometry']['type'])
-			if (len(coords.shape) != 2):
-				coords=np.array(coords[0])
-				print('      - ',coords[0].shape)
-				#continue
-			
-		
-			#if int(feature['id'])==100:
-			ax.plot(coords[:,0], coords[:,1])
-		
-		if cnt>2500:
-			break
-		
-	#	print(feature['properties']['STATEFP'], ' - ', feature['properties']['NAME'], '\t\t\t', (minLng+maxLng)/2, (minLat+maxLat)/2)
-		#ax.text((minLat+maxLat)/2, (minLng+maxLng)/2, feature['properties']['NAME'], fontsize=4)
-		#ax.text((minLng+maxLng)/2, (minLat+maxLat)/2, feature['properties']['NAME'], fontsize=18)
-	
-	plt.show()
+# plot the shapes in the shapefile <testFile> with matplotlib
+testFile = '../data/shapefiles-wi_counties/cb_2015_55_cousub_500k.shp'
+testFile = '../data/shapefiles-us_zips/cb_2017_us_zcta510_500k.shp'
+with fiona.open(testFile, 'r') as src:
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cnt = 0
+    for feature in src:
+        for poly in feature['geometry']['coordinates']:
+            coords = np.array(poly).squeeze()
+            cnt += 1
+            print(feature)
+            #print(feature['id'], coords.shape, feature['properties']['COUNTYFP'], feature['properties']['NAME'])
+            if len(coords.shape) != 2:
+                coords = np.array(coords[0])
+                print('*** Unusual shape?  ', coords[0].shape, '  ***')
+
+            ax.plot(coords[:,0], coords[:,1])
+
+        if cnt > 1000:
+            break
+
+    #	print(feature['properties']['STATEFP'], ' - ', feature['properties']['NAME'], '\t\t\t', (minLng+maxLng)/2, (minLat+maxLat)/2)
+        #ax.text((minLat+maxLat)/2, (minLng+maxLng)/2, feature['properties']['NAME'], fontsize=4)
+        #ax.text((minLng+maxLng)/2, (minLat+maxLat)/2, feature['properties']['NAME'], fontsize=18)
+
+    plt.show()
--- python/shp2gjson.py
+++ python/shp2gjson.py
@@ -6,7 +6,8 @@ import os
 # shapefile is source .shp file/dir (pass without the .shp)
 # recFiltFunc is a function to call on each element/record in shapefile to filter out unwanted elements
 # boundingBox is the bounding box of shape elements to keep - at least one point in shape must be inside boundingBox
-# dataProp is
+# dataProp is the property in the shapefile that we care about? (like 'ZCTA5CE10')
+# newDataProp is what we'll rename dataProp in the output (like 'ZCTA5CE10'->'zip')
 def translate_shapefile(shapeFile, recFiltFunc, boundingBox, dataProp, newDataProp):
     if os.path.isfile(shapeFile+'.json'):
         os.remove(shapeFile+'.json')
@@ -18,16 +19,15 @@ def translate_shapefile(shapeFile, recFiltFunc, boundingBox, dataProp, newDataPr
         meta['schema']['properties'] = collections.OrderedDict([(newDataProp, 'str:5'), ('id', 'int')])
         meta['driver'] = 'GeoJSON'
         with fiona.open(shapeFile+'.json', 'w', **meta) as sink:
-            filtSrc = filter(recFiltFunc, src)
-            for rec in filtSrc:
+            for rec in filter(recFiltFunc, src):
                 # not polygon means the multi-level/nested polygon thing. This removes those complicated compound shapes
                 if rec['geometry']['type'] != 'Polygon':
                     rec['geometry']['type'] = 'Polygon'
-                    newCoords=[]
+                    newCoords = []
 
                     # simplify multipolygon to polygon:
-                    # if polygon has negative space, the first element is the whole, skip the other pieces
-                    # if polygona consiss of multiple pieces we include all of those
+                    # if polygon has negative space, the first element is the outer bound, skip other/interior pieces
+                    # if polygon consists of multiple, separate pieces (like islands) we include all of those
                     for idx in range(len(rec['geometry']['coordinates'])):
                         newCoords.append(rec['geometry']['coordinates'][idx][0])
 
@@ -46,6 +46,10 @@ def translate_shapefile(shapeFile, recFiltFunc, boundingBox, dataProp, newDataPr
 
 # true if any point of geomElem's polygon is inside the bounding box (intersect, not necessarily fully within/contained)
 def polygon_intersects_box(geomElem, boundingBox):
+    # if no box, don't filter
+    if boundingBox is None:
+        return True
+
     for geomPolygon in geomElem['coordinates']:
         polygonCoords = np.array(geomPolygon)
         if any( map(lambda x: (boundingBox[0][0] < x[0] < boundingBox[1][0] and x[1] < boundingBox[1][1] < boundingBox[0][1]), polygonCoords) ):
@@ -56,5 +60,6 @@ def polygon_intersects_box(geomElem, boundingBox):
 
 if __name__ == "__main__":
     bBox = [[-90, 42.5], [-89, 43.5]]
-    shapeFile = "../data/WI_County/cb_2015_us_zcta510_500k"
+    bBox = None
+    shapeFile = "../data/shapefiles-wi_counties/cb_2015_us_zcta510_500k"
     recList = translate_shapefile(shapeFile, lambda x: True , bBox, 'ZCTA5CE10', 'zip')
